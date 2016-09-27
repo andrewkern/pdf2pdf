9fb2d3e2d73b5fb7809d1b676b2d2c3da5b124a6
Jeffrey Ross-Ibarra <rossibarra@ucdavis.edu>
2016-09-27 10:04:10 -0700

    progress bar

--- pdf2pdf.sh
+++ pdf2pdf.sh
@@ -2,8 +2,18 @@
 
 #pdf2pdf.sh
 
-
-
-sleep 6
+sleep 1
+echo -ne '###                     (16%)\r'
+sleep 1
+echo -ne '######                  (33%)\r'
+sleep 1
+echo -ne '#########'              (50%)\r'
+sleep 1
+echo -ne '############            (66%)\r'
+sleep 1
+echo -ne '###############         (83%)\r'
+sleep 1
+echo -ne '##################     (100%)\r'
+echo -ne '\n'
 
 echo "conversion of $1 complete"
