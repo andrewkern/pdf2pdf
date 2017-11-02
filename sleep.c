c28e60756e265252834d2a357df24db16d090cf6
Kern Lab <kern-lab@users.noreply.github.com>
2017-11-02 13:16:39 -0400

    merging yaniv's changes into sleep.c

--- sleep.c
+++ sleep.c
@@ -6,15 +6,13 @@ int main()
 {
   
 usleep(833333);	
-cout	<< "###                     (16%)\r" << std::flush;
+cout	<< "###                     (20%)\r" << std::flush;
 usleep(833333);	
-cout	<< "######                  (33%)\r" << std::flush;
+cout	<< "######                  (40%)\r" << std::flush;
 usleep(833333);	
-cout	<< "#########               (50%)\r" << std::flush;
+cout	<< "#########               (60%)\r" << std::flush;
 usleep(833333);	
-cout	<< "############            (66%)\r" << std::flush;
-usleep(833333);	
-cout	<< "###############         (83%)\r" << std::flush;
+cout	<< "##############          (80%)\r" << std::flush;
 usleep(833333);	
 cout	<< "##################     (100%)\r" << std::flush;
 cout	<< "\n";
