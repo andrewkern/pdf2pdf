3f6d04af015f35c61aee10ac6e1b8a6f0e09eee9
Mark T. Holder <mtholder@gmail.com>
2018-05-04 17:02:59 -0500

    various ui tweaks

--- pdf2pdf.sh
+++ pdf2pdf.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/env bash
 
 #pdf2pdf.sh
+if test $# -eq 0 ; then
+	echo "Error: pdf required for conversion to pdf"
+	exit 1
+fi
+
+numerrors=0
 
 for file in "$@"
 do
 	if file --mime-type "$file" | grep -q pdf$; then
-	    echo "Converting $file"
+		echo "Converting $file"
 
 		if [ -f ./fastpdf ]; then
 			./fastpdf
 		else
-   			echo "File fastpdf does not exist. Please compile (see readme)."
+			echo "File fastpdf does not exist. Please compile (see readme)."
 			break;
 		fi	
 
 		# important, update time-stamp
-		touch "$file"
+		touch "${file}"
 		
 		# rasterize and subtly degrade quality
-		convert -density 95 "$file" -quality 95 $RANDOM.pdf
+		outfile=$RANDOM.pdf
+		convert -density 95 "${file}" -quality 95 $outfile
 
-		echo -e 'Conversion completed.\n'
+		echo -e "Conversion to ${outfile} completed.\n"
 
 	else
-	  	echo -e "Warning: Cannot convert $file, not of type pdf\n"
+		echo -e "Warning: Cannot convert $file, not of type pdf\n"
+		numerrors=`expr 1 + $numerrors`
 	fi
 
 done
+
+exit $numerrors
